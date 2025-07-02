a66669d13a817b8f3328307d3a76e30f00f4cf8d
RadicalEddward <RadicalEddward@protonmail.com>
2025-07-02 16:12:12 +0200

    redid folder structure so i'm able to properly set up backend using node and express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -9,8 +10,14 @@ const port = 3000;
 
 app.use(express.static(__dirname + "/../public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 })
 
 app.listen(port, () => {
