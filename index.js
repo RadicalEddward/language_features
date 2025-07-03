04e7b0434d25b7de40278ed4b2ad07c50aa006f6
RadicalEddward <RadicalEddward@protonmail.com>
2025-07-03 10:56:36 +0200

    added ability to make posts, as well as to delete any posts made

--- index.js
+++ index.js
@@ -1,24 +1,79 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import methodOverride from "method-override";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/../public"));
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+// get routes for page naivigation
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.render("index.ejs");
 })
 
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+})
+
+app.get("/discussion", (req, res) => {
+  res.render("discussion.ejs");
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+})
+
+// main page
+// post route for correlation submission
 app.post("/submit", (req, res) => {
   console.log(req.body);
+  res.render("index.ejs", { message: "Form submitted successfully!" });
 })
+
+
+// discussion page
+// post route for discussion post submission
+const posts = [];
+
+// add posts to display them
+app.post("/discussion", (req, res) => {
+  req.body.id = posts.length; // Assign an ID to the post
+
+  posts.push(req.body);
+
+  res.render("discussion.ejs", {
+    message: "Post submitted successfully!",
+    posts: posts,
+  });
+})
+
+// delete post route
+app.delete("/discussion", (req, res) => {
+  console.log("Delete post request received");
+  // const postId = parseInt(req.params.id, 10);
+  const postId = Object.keys(req.body)[0].at(-1); // Get the post ID from the request body
+  console.log(Object.keys(req.body)[0].at(-1));
+
+  posts.splice(postId, 1, null); // Remove the post from the array
+  console.log(`Post with ID ${postId} deleted`);
+  console.log(posts);
+  // res.redirect("/discussion");
+  res.render("discussion.ejs", {
+    message: "Post deleted successfully!",
+    posts: posts,
+  });
+})
+
+
+// edit post route
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
